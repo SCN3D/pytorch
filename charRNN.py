b72f179c2880635b53bc12c86cfad6cf15545264
scn3d <scn3d@mail.missouri.edu>
2018-02-20 02:30:02 -0600

    1

---
+++ charRNN.py
@@ -0,0 +1,197 @@
+
+from __future__ import unicode_literals, print_function, division
+from io import open
+from numpy import genfromtxt
+import numpy as np
+import glob
+import time
+import math
+import unicodedata
+import string
+import torch
+
+
+def findFiles(path): return glob.glob(path)
+
+print(findFiles('/home/dlg/JL/siteng/data/train/part_0.csv'))
+
+all_letters = 'ARNDCEQGHILKMFPSTWYV'
+n_letters = len(all_letters)
+
+# Turn a Unicode string to plain ASCII, thanks to http://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+
+# Build the category_lines dictionary, a list of names per language
+category_lines = {}
+all_categories = []
+category =  '1'
+all_categories.append(category)
+category =  '2'
+all_categories.append(category)
+n_categories = 2
+
+target = genfromtxt('/home/dlg/JL/siteng/data/train/part_0.csv',dtype=str, delimiter=',',usecols=(1))
+data = genfromtxt('C:/Users/Cai/Desktop/pytorch/part_0.csv', dtype=str,delimiter=',',usecols=(0))
+
+
+##################################
+
+######################################################################
+# Turning Names into Tensors
+
+
+
+# Find letter index from all_letters, e.g. "a" = 0
+def letterToIndex(letter):
+    return all_letters.find(letter)
+
+# Turn a line into a <line_length x 1 x n_letters>,
+# or an array of one-hot letter vectors
+def lineToTensor(line):
+    tensor = torch.zeros(len(line), 1, n_letters)
+    for li, letter in enumerate(line):
+        tensor[li][0][letterToIndex(letter)] = 1
+    return tensor
+
+
+######################################################################
+# Creating the Network
+
+
+import torch.nn as nn
+from torch.autograd import Variable
+
+class RNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size):
+        super(RNN, self).__init__()
+
+        self.hidden_size = hidden_size
+
+        self.i2h = nn.Linear(input_size + hidden_size, hidden_size)
+        self.i2o = nn.Linear(input_size + hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        combined = torch.cat((input, hidden), 1)
+        hidden = self.i2h(combined)
+        output = self.i2o(combined)
+        output = self.softmax(output)
+        return output, hidden
+
+    def initHidden(self):
+        return Variable(torch.zeros(1, self.hidden_size))
+
+n_hidden = 128
+rnn = RNN(n_letters, n_hidden, n_categories)
+
+
+######################################################################
+
+def categoryFromOutput(output):
+    top_n, top_i = output.data.topk(1) # Tensor out of Variable with .data
+    category_i = top_i[0][0]
+    return all_categories[category_i], category_i
+
+
+######################################################################
+
+randomChoice(all_categories)
+criterion = nn.NLLLoss()
+
+learning_rate = 0.005 # If you set this too high, it might explode. If too low, it might not learn
+
+def train(category_tensor, line_tensor):
+    hidden = rnn.initHidden()
+
+    rnn.zero_grad()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    loss = criterion(output, category_tensor)
+    loss.backward()
+
+    # Add parameters' gradients to their values, multiplied by learning rate
+    for p in rnn.parameters():
+        p.data.add_(-learning_rate, p.grad.data)
+
+    return output, loss.data[0]
+
+
+######################################################################
+# training
+
+
+
+
+n_iters = 100000
+print_every = 5000
+plot_every = 1000
+
+
+
+# Keep track of losses for plotting
+current_loss = 0
+all_losses = []
+
+def timeSince(since):
+    now = time.time()
+    s = now - since
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+start = time.time()
+i = 0
+for iter in range(1, n_iters + 1):
+    ## sample
+    indexRange = len(data)
+    randomSampleIndex = np.random.choice(indexRange)
+    category = target[randomSampleIndex]
+    line = data[randomSampleIndex]
+    category_tensor = Variable(torch.LongTensor([all_categories.index(category)]))
+    line_tensor = Variable(lineToTensor(line))
+
+    ########
+
+
+    output, loss = train(category_tensor, line_tensor)
+    current_loss += loss
+
+    # Print iter number, loss, name and guess
+    if iter % print_every == 0:
+        guess, guess_i = categoryFromOutput(output)
+        correct = '✓' if guess == category else '✗ (%s)' % category
+        print('%d %d%% (%s) %.4f %s / %s %s' % (iter, iter / n_iters * 100, timeSince(start), loss, line, guess, correct))
+
+    # Add current loss avg to list of losses
+    if iter % plot_every == 0:
+        all_losses.append(current_loss / plot_every)
+        current_loss = 0
+
+
+
+
+######################################################################
+# Running on User Input
+# ---------------------
+#
+
+def predict(input_line, n_predictions=3):
+    print('\n> %s' % input_line)
+    output = evaluate(Variable(lineToTensor(input_line)))
+
+    # Get top N categories
+    topv, topi = output.data.topk(n_predictions, 1, True)
+    predictions = []
+
+    for i in range(n_predictions):
+        value = topv[0][i]
+        category_index = topi[0][i]
+        print('(%.2f) %s' % (value, all_categories[category_index]))
+        predictions.append([value, all_categories[category_index]])
